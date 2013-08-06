f2bf12b06d76acfeac70ebe703dba0402e5fd38c
Justin Bull <me@justinbull.ca>
2013-08-06 18:54:00 -0400

    Generate rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doorprize::Application.config.secret_key_base = 'da4a3f861b2eabc0be3fc0ef74b2ea3f7ba07d9d43ad9596ff411866831ddead6c7e6eddee5800ccd02563044aca016517d35bb2068b0d247496f39329a37bca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Doorprize::Application.config.session_store :cookie_store, key: '_doorprize_session'
