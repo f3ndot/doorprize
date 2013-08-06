f85d79d419e2ec50b40ec217f1bb126b8b3bcb06
Justin Bull <me@justinbull.ca>
2013-08-06 19:46:46 -0400

    Add incident scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Doorprize::Application.routes.draw do
+  resources :incidents
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
