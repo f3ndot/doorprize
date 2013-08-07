19b0e98d46a20e32cb0209ed49f6882a0d2bdb3a
Justin Bull <me@justinbull.ca>
2013-08-06 20:52:50 -0400

    Make incident root, add menu helper

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Doorprize::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'incidents#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -41,7 +41,7 @@ Doorprize::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
