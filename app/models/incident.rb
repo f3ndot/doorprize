b4effb9425dce02c55bba512e6ad547bda692fc4
Justin Bull <me@justinbull.ca>
2013-08-10 18:18:14 -0400

    Validate severity range and test for it. See #1

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,5 @@
 class Incident < ActiveRecord::Base
+
+  validates :severity, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
 end
--- test/models/incident_test.rb
+++ test/models/incident_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class IncidentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "severity score range" do
+    incident_one = incidents(:one)
+    incident_two = incidents(:two)
+    min_range = 0
+    max_range = 10
+
+    assert incident_one.severity.between?(min_range, max_range)
+    assert incident_two.severity.between?(min_range, max_range)
+
+    incident_one.severity = -1
+    incident_two.severity = 11
+
+    refute incident_one.severity.between?(min_range, max_range)
+    refute incident_two.severity.between?(min_range, max_range)
+  end
 end
