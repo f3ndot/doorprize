87d9f0bee099c278f3560c9a05ba4bced28e3ab7
Justin Bull <me@justinbull.ca>
2013-08-11 13:03:50 -0400

    Store license plate in uppercase format

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,12 @@
 class Car < ActiveRecord::Base
+
   belongs_to :incident
+
+  validates :description, presence: true
+
+  # Make sure that license plates are stored in uppercase
+  def licence_plate=(plate)
+    write_attribute :licence_plate, plate.upcase
+  end
+
 end
