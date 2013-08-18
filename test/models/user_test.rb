8da71510ccf6d73da9cdc58dafea693802c2c50d
Justin Bull <me@justinbull.ca>
2013-08-18 14:00:20 -0400

    Add *some* user tests. Pretty lacking, though

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @omniauth_users = []
+    @omniauth_users.push users(:twitter)
+    @omniauth_users.push users(:github)
+    @omniauth_users.push users(:facebook)
+    @omniauth_users.push users(:google)
+  end
+
+  test "omniauth users don't have to set password" do
+    @omniauth_users.each do |user|
+      refute user.password_required?, 'password is not required for omniauth users'
+    end
+  end
+
+  test "passwordless users don't have to check current password" do
+    params = {name: 'New Name', email: 'new_email@example.com'}
+
+    user = users(:one)
+    refute user.update_with_password(params), 'traditional user was able to update attributes without password'
+
+    @omniauth_users.each do |user|
+      assert_not_equal params[:name], user.name
+      assert_not_equal params[:email], user.email
+      user.update_with_password(params)
+      assert_equal params[:name], user.name
+      assert_equal params[:email], user.email
+    end
+  end
+
+  test "user names are required" do
+    user = users(:one)
+    assert user.valid?
+
+    user.name = nil
+    refute user.valid?
+
+    user.name = ''
+    refute user.valid?
+  end
 end
