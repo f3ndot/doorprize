d1b569c1c91552d8b117faf30a61b26065839932
Justin Bull <me@justinbull.ca>
2013-08-20 20:02:11 -0400

    Inform user that name and contact fields will be private

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def legitimacy_score_popover_text
     "<p><strong>Doored.ca</strong> is a <em>crowdsourced</em> stats database with all of its data entered by users. As a result, many reports could be fraudulent or misrepresented.</p><p>A <strong>legitimacy or reliability score</strong> exists to automatically determine which data points are most likely to be accurate.</p><p>An admin or moderator, at a later time, may manually verify the details of a report and set a score override.</p>".html_safe
   end
+
+  def hidden_from_public_hint
+    '<span class="text-info"><i class="icon-eye-close"></i> This field will be hidden from public</span>'.html_safe
+  end
 end
