2d61f230c373ee87fee5bdded4345828e0b523f2
Justin Bull <me@justinbull.ca>
2013-08-20 22:21:22 -0400

    show photo in edit screen with custom input

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -43,9 +43,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fill => [160, 160]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
