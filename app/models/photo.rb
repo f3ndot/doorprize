ccff94f5066d488347d4e3a1314fb2d1abbcd6b6
Justin Bull <me@justinbull.ca>
2013-08-20 21:02:51 -0400

    resize, whitelist ext, destroy image callback

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,12 @@ class Photo < ActiveRecord::Base
   belongs_to :incident
   mount_uploader :image, PhotoUploader
 
+  before_destroy :delete_image_from_store
+
+  protected
+
+  def delete_image_from_store
+    remove_image
+  end
+
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -28,6 +28,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  process :resize_to_fit => [800, 800]
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
@@ -42,9 +43,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
